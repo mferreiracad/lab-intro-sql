fa3e03c3288020a316ad36feb9119d3386cd0939
mFERREIRAcad <mferreiracad@gmail.com>
2022-06-21 17:46:10 +0100

    sql lab done

---
+++ lab_sql.sql
@@ -0,0 +1,15 @@
+use sakila;
+SELECT * FROM actor;
+SELECT * FROM film;
+SELECT * FROM customer;
+
+SELECT title FROM film;
+
+SELECT DISTINCT(name) FROM language;
+
+SELECT COUNT(store_id) FROM store;
+
+SELECT Count(staff_id) FROM staff;
+
+SELECT first_name FROM staff;
+
